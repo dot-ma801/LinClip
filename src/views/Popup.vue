66fd6fd5c9f52672aa474a730c053c86eef17578
dot-ma801 <dot.ma801@gmail.com>
2025-02-11 18:50:18 +0900

    [update] 他のボタンにも水平展開

--- src/views/Popup.vue
+++ src/views/Popup.vue
@@ -5,17 +5,21 @@ import { ref } from 'vue';
 
 type CopyStatus = 'waiting' | 'loading' | 'success' | 'fail';
 
-const copyStatus = ref<CopyStatus>('waiting');
+const copyStatus = ref<{ markdown: CopyStatus; blob: CopyStatus; text: CopyStatus }>({
+	markdown: 'waiting',
+	blob: 'waiting',
+	text: 'waiting',
+});
 
 const onclickMarkdown = async () => {
 	const { title, url } = await getTabInfo();
 	const markdownText = `[${title}](${url})`;
 	const blob = {
-		'text/plain': new Blob([markdownText], { type: 'text/plai' }),
+		'text/plain': new Blob([markdownText], { type: 'text/plain' }),
 	};
 	const clipboardItem = new ClipboardItem(blob);
 
-	copyToClipboardWithStatus(clipboardItem);
+	copyToClipboardWithStatus(clipboardItem, 'markdown');
 };
 
 const onclickBlob = async () => {
@@ -28,7 +32,7 @@ const onclickBlob = async () => {
 	};
 	const clipboardItem = new ClipboardItem(blob);
 
-	copyToClipboardWithStatus(clipboardItem);
+	copyToClipboardWithStatus(clipboardItem, 'blob');
 };
 
 const onclickText = async () => {
@@ -39,44 +43,62 @@ const onclickText = async () => {
 	};
 	const clipboardItem = new ClipboardItem(blob);
 
-	copyToClipboardWithStatus(clipboardItem);
+	copyToClipboardWithStatus(clipboardItem, 'text');
 };
 
-const copyToClipboardWithStatus = (clipboardItem: ClipboardItem) => {
-	copyStatus.value = 'loading';
+const copyToClipboardWithStatus = (clipboardItem: ClipboardItem, statusKey: keyof typeof copyStatus.value) => {
+	copyStatus.value[statusKey] = 'loading';
 	copyToClipboard(
 		clipboardItem,
 		() => {
-			copyStatus.value = 'success';
+			copyStatus.value[statusKey] = 'success';
 			setTimeout(() => {
-				copyStatus.value = 'waiting';
+				copyStatus.value[statusKey] = 'waiting';
 			}, 3000);
 		},
 		() => {
-			copyStatus.value = 'fail';
+			copyStatus.value[statusKey] = 'fail';
 			setTimeout(() => {
-				copyStatus.value = 'waiting';
+				copyStatus.value[statusKey] = 'waiting';
 			}, 3000);
 		}
 	);
+};
+
+const buttons = {
+	markdown: {
+		btnText: 'Copy Link as Markdown',
+		prependIcon: 'mdi-alpha-m-box',
+		clickHandler: onclickMarkdown,
+	},
+	blob: {
+		btnText: 'Copy Link as Blob',
+		prependIcon: 'mdi-code-braces',
+		clickHandler: onclickBlob,
+	},
+	text: {
+		btnText: 'Copy Title as Text',
+		prependIcon: 'mdi-format-text',
+		clickHandler: onclickText,
+	},
 };
 </script>
 
 <template>
 	<div class="button-container">
-		<v-btn prepend-icon="mdi-alpha-m-box" :disabled="copyStatus == 'loading'" @click="onclickMarkdown">
-			Copy Link as Markdown
-			<template v-slot:append v-if="copyStatus == 'success'">
-				<v-icon color="success">mdi-check-circle</v-icon>
-			</template>
-			<template v-slot:append v-if="copyStatus == 'fail'">
-				<v-icon color="error">mdi-close-circle</v-icon>
-			</template>
-		</v-btn>
-		<v-btn prepend-icon="mdi-code-braces" @click="onclickBlob"> Copy Link as Blob</v-btn>
-		<v-btn prepend-icon="mdi-format-text" @click="onclickText"> Copy Title as Text</v-btn>
+		<template v-for="(item, key) in buttons">
+			<v-btn :prepend-icon="item.prependIcon" :disabled="copyStatus[key] == 'loading'" @click="item.clickHandler">
+				{{ item.btnText }}
+				<template v-slot:append v-if="copyStatus[key] == 'success'">
+					<v-icon color="success">mdi-check-circle</v-icon>
+				</template>
+				<template v-slot:append v-if="copyStatus[key] == 'fail'">
+					<v-icon color="error">mdi-close-circle</v-icon>
+				</template>
+			</v-btn>
+		</template>
 		<v-divider></v-divider>
-		<v-btn prepend-icon="mdi-plus" stacked> add another pattern </v-btn>
+		<v-btn prepend-icon="mdi-plus" stacked :disabled="true"> add another pattern </v-btn>
 	</div>
 </template>
 <style scoped>
