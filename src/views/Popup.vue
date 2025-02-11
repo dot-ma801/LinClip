6179a625f194c2aae94a038fe2cc6f6b9991fbea
dot-ma801 <dot.ma801@gmail.com>
2025-02-11 17:27:43 +0900

    [add] PopUpを仮実装

---
+++ src/views/Popup.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+import { getTabInfo } from '@/Utils/tabs';
+import { copyToClipbord } from '@/Utils/copy';
+
+const onclickMarkdown = async () => {
+	const { title, url } = await getTabInfo();
+	const markdownText = `[${title}](${url})`;
+	const blob = {
+		'text/plain': new Blob([markdownText], { type: 'text/plain' }),
+	};
+	const clipbordItem = new ClipboardItem(blob);
+
+	copyToClipbord(clipbordItem);
+};
+
+const onclickBlob = async () => {
+	const { title, url } = await getTabInfo();
+
+	const blob = {
+		'text/html': new Blob([`<a href="${url}">${title}</a>`], {
+			type: 'text/html',
+		}),
+	};
+	const clipbordItem = new ClipboardItem(blob);
+
+	copyToClipbord(clipbordItem);
+};
+
+const onclickText = async () => {
+	const { title } = await getTabInfo();
+	const text = `${title}`;
+	const blob = {
+		'text/plain': new Blob([text], { type: 'text/plain' }),
+	};
+	const clipbordItem = new ClipboardItem(blob);
+
+	copyToClipbord(clipbordItem);
+};
+</script>
+
+<template>
+	<div class="button-container">
+		<v-btn prepend-icon="mdi-alpha-m-box" @click="onclickMarkdown"> Copy Link as Markdown</v-btn>
+		<v-btn prepend-icon="mdi-code-braces" @click="onclickBlob"> Copy Link as Blob</v-btn>
+		<v-btn prepend-icon="mdi-format-text" @click="onclickText"> Copy Title as Text</v-btn>
+		<v-divider></v-divider>
+		<v-btn prepend-icon="mdi-plus" stacked> add another pattern </v-btn>
+	</div>
+</template>
+<style scoped>
+.button-container {
+	padding: 30px;
+	display: flex;
+	flex-direction: column;
+	gap: 10px;
+}
+
+/* v-btnがTextをすべて大文字に変える仕様なので、その対応 */
+* {
+	text-transform: none !important;
+}
+</style>
