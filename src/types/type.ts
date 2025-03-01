0221c4702cf7740ce8760d51c390b3be6f3bab18
dot-ma801 <dot.ma801@gmail.com>
2025-03-01 23:53:39 +0900

    [add] 入力を受け付ける予定のjsonの型を宣言

--- src/types/type.ts
+++ src/types/type.ts
@@ -1,17 +1,52 @@
-export type Setting = {
-  urlPattern: string;
-  copyOptions: CopyOption[];
-  selectors?: Selector[];
-}[];
+interface DataItem {
+  url_regex: string;
+  btns: Button[];
+}
 
-export type CopyOption = {
-  name: string;
-  format: 'markdown' | 'blob' | 'text';
-  template: string;
+interface Button {
+  btn_name: string;
+  type: 'text' | 'html' | 'markdown';
+  pattern: string;
+}
+
+export type ConfigData = {
+  data: DataItem[];
 };
 
-type Selector = {
-  name: string;
-  type: 'css' | 'id' | 'class' | 'meta' | 'js';
-  query: string;
-};
+// 動作確認用の宣言
+const configJson = `{
+  "data": [
+    {
+      "url_regex": "https://**",
+      "btns": [
+        {
+          "btn_name": "btn1",
+          "type": "text",
+          "pattern": "red#{$url$1} - {$url$2}"
+        },
+        {
+          "btn_name": "btn2",
+          "type": "html",
+          "pattern": "blue#{$url$1} - {tab}"
+        },
+                {
+          "btn_name": "btn2",
+          "type": "html",
+          "pattern": "blue#{$url$1} - {tab}"
+        }
+      ]
+    },
+    {
+      "url_regex": "https://**",
+      "btns": [
+        {
+          "btn_name": "btn3",
+          "type": "markdown",
+          "pattern": "red#{$url$1} - {$url$2}"
+        }
+      ]
+    }
+  ]
+}`;
+
+export const configForTest: ConfigData = JSON.parse(configJson);
